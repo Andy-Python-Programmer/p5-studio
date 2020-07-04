bdae491eb693c034bc9d99cee6ac43e6bb11e748
Manmeet Maan <manmeetmann2003@outlook.com>
2020-07-04 13:58:46 +0530

    use async functions

--- creator/src/index.js
+++ creator/src/index.js
@@ -12,49 +12,50 @@ const readline = require('readline');
 const staticConf = require("./configs/staticConf.js");
 const server = require("./server/server.js");
 
-const existindex = fs.existsSync("index.html");
-
-// Clean Screen
-
-const blank = '\n'.repeat(process.stdout.rows);
-
-console.log(blank);
-
-readline.cursorTo(process.stdout, 0, 0);
-readline.clearScreenDown(process.stdout);
-
 // Rest of code
 
-function config() {
-    inquirer
-    .prompt([
-        {
-            type: "text",
-            name: "name",
-            message: "What is the name of the project ?",
-            default: path.basename(process.cwd()),
-        },
-        {
-            type: "list",
-            name: "type",
-            message: "Do you want to integrate node.js with the p5.js app ?",
-            choices: [
-                "static",
-                "node"
-            ]
-        }
-    ])
-    .then(answers => {
-        switch(answers.type) {
-            case "static":
-                staticConf(config, answers.name, process.cwd());
-                break;
-        }
-    })
+async function config() {
+    const answers = await inquirer
+        .prompt([
+            {
+                type: "text",
+                name: "name",
+                message: "What is the name of the project ?",
+                default: path.basename(process.cwd()),
+            },
+            {
+                type: "list",
+                name: "type",
+                message: "Do you want to integrate node.js with the p5.js app ?",
+                choices: [
+                    "static",
+                    "node"
+                ]
+            }
+        ]);
+
+    switch (answers.type) {
+        case "static":
+            await staticConf(config, answers.name, process.cwd());
+            break;
+    }
 }
 
-if (existindex) {
-    inquirer
+async function main() {
+    const existindex = fs.existsSync("index.html");
+
+    // Clean Screen
+    readline.cursorTo(process.stdout, 0, 0);
+    readline.clearScreenDown(process.stdout);
+
+    if (!existindex) {
+        // simply create new project
+        await config();
+        return;
+    }
+
+    // Ask to overtime the file
+    const answers = await inquirer
         .prompt([
             {
                 type: "confirm",
@@ -62,15 +63,14 @@ if (existindex) {
                 message: "index.html already exists! Would you like to open live server here ?",
                 default: false,
             }
-        ])
-        .then(answers => {
-            if (answers.overwrite) {
-                server(process.cwd())
-            } else{
-                console.log("Goodbye :)")
-            }
-        })
-} 
-else{
-    config();
+        ]);
+
+    if (!answers.overwrite) {
+        console.log("Goodbye :)");
+        return;
+    }
+
+    server(process.cwd());
 }
+
+main();
