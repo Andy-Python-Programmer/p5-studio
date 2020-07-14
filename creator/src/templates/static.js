54f933f89e79929ebb82e186ee573fc263f48cf7
Manmeet Maan <manmeetmann2003@outlook.com>
2020-07-14 15:10:06 +0530

    Fix template

--- creator/src/templates/static.js
+++ creator/src/templates/static.js
@@ -9,8 +9,8 @@ module.exports = (name) => ({
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="p5.js"></script>
-    <script src="p5.sound.min.js"></script>
+    <script src="libs/p5.js"></script>
+    <script src="libs/p5.sound.min.js"></script>
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>${name}</title>
 </head>
