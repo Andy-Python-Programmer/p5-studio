11c75501993661305b2b7a860769741dcc9ff41b
Manmeet Maan <manmeetmann2003@outlook.com>
2020-07-14 13:21:52 +0530

    Use static files instead of cdn

--- creator/src/templates/static.js
+++ creator/src/templates/static.js
@@ -5,8 +5,8 @@ module.exports = name => ({
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.10.2/p5.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.10.2/addons/p5.sound.min.js"></script>
+    <script src="p5.js"></script>
+    <script src="p5.sound.min.js"></script>
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>${name}</title>
 </head>
